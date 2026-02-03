bac17e0d2fbc3b8fe366130f7ba1cedd239a59ba
tlc120 <tlc1@kent.ac.uk>
2026-02-03 12:39:45 +0000

    Part One Finish

--- camel.py
+++ camel.py
@@ -1,1 +1,4 @@
-# Enter your code here
+import re
+name = input("Please enter your name. ")
+name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+print(name)
--- coffee.py
+++ coffee.py
@@ -1,1 +1,16 @@
-#Your code goes here
+while True:
+    coins1 = int(input("Please insert a coin. "))
+    coins2 = input(f"You have insert {coins1}. Is that the right amount coins? ")
+    if coins2 == "NO":
+        continue
+    elif coins2 == "Yes":
+        break
+match coins3:
+    case "Picard" | "Riker" | "La Forge":
+        print("Command Division")
+    case "Data" | "Worf":
+        print("Operations Division")
+    case "Crusher" | "Troi":
+        print("Sciences Division")
+    case _:
+        print("Character not found. Please enter a main TNG character.")
